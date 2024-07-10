f0a2eacb638f19225a395106fb4a8a43550c37b1
Paschalis Valtsianis <valitsianius@hotmail.com>
2024-07-10 17:35:42 +0300

    update files regarding google cloud

--- src/googleCloudStorage.ts
+++ src/googleCloudStorage.ts
@@ -1,13 +1,17 @@
-import { Storage } from '@google-cloud/storage'
-import path from 'path'
+import dotenv from "dotenv";
+import { Storage } from "@google-cloud/storage";
+import path from "path";
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Initialize Google Cloud Storage
 const storage = new Storage({
-    keyFilename: path.join(process.cwd(), 'google-cloud-key.json'),
-    projectId: 'wedding-photos-429010', // Replace with your project ID
-})
+  keyFilename: path.join(process.cwd(), "google-cloud-key.json"),
+  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID, // Use environment variable for project ID
+});
 
-const bucketName = 'photo-bucket_2024' // Replace with your bucket name
-const bucket = storage.bucket(bucketName)
+const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME || ""; // Use environment variable for bucket name
+const bucket = storage.bucket(bucketName);
 
-export { bucket }
+export { bucket };
