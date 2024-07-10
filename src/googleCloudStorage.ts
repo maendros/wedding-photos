1d8e11b5ea1adab90612da5581ee32dfd038fed1
Paschalis Valtsianis <valitsianius@hotmail.com>
2024-07-10 18:00:43 +0300

    Adjusting google creds

--- src/googleCloudStorage.ts
+++ src/googleCloudStorage.ts
@@ -5,13 +5,22 @@ import path from "path";
 // Load environment variables from .env file
 dotenv.config();
 
+const {
+  GOOGLE_APPLICATION_CREDENTIALS,
+  GOOGLE_CLOUD_BUCKET_NAME,
+  GOOGLE_CLOUD_PROJECT_ID,
+} = process.env;
+
 // Initialize Google Cloud Storage
 const storage = new Storage({
-  keyFilename: path.join(process.cwd(), "google-cloud-key.json"),
-  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID, // Use environment variable for project ID
+  keyFilename: path.join(
+    process.cwd(),
+    GOOGLE_APPLICATION_CREDENTIALS || "google-cloud-key.json"
+  ),
+  projectId: GOOGLE_CLOUD_PROJECT_ID, // Use environment variable for project ID
 });
 
-const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME || ""; // Use environment variable for bucket name
+const bucketName = GOOGLE_CLOUD_BUCKET_NAME || ""; // Use environment variable for bucket name
 const bucket = storage.bucket(bucketName);
 
 export { bucket };
