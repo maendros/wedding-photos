cc1ce3e86f7e67d77ba6fc05827c665dbe25407f
Paschalis Valtsianis <PValtsianis@i4.energy>
2024-07-10 13:50:31 +0300

    update storage

--- src/googleCloudStorage.ts
+++ src/googleCloudStorage.ts
@@ -4,10 +4,10 @@ import path from 'path'
 // Initialize Google Cloud Storage
 const storage = new Storage({
     keyFilename: path.join(process.cwd(), 'google-cloud-key.json'),
-    projectId: 'your_project_id',
+    projectId: 'wedding-photos-429010', // Replace with your project ID
 })
 
-const bucketName = 'your_bucket_name'
+const bucketName = 'photo-bucket_2024' // Replace with your bucket name
 const bucket = storage.bucket(bucketName)
 
 export { bucket }
