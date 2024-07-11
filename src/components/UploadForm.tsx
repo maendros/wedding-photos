f8c41466a91638dfe98f80bdc95aa68126e50029
Paschalis Valtsianis <valitsianius@hotmail.com>
2024-07-11 14:06:41 +0300

    Fix responsive for upload

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -20,13 +20,14 @@ const UploadForm: React.FC<UploadFormProps> = ({
   const [uploadProgress, setUploadProgress] = useState(0);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const [fileError, setFileError] = useState<string | null>(null);
+  const [fileName, setFileName] = useState<string | null>(null); // State to hold file name
 
   const formik = useFormik<FormValues>({
     initialValues: {
       file: null,
     },
     validationSchema: Yup.object({
-      file: Yup.mixed().required("A file is required"),
+      file: Yup.mixed().required("Απαιτείται Φωτογραφία"),
     }),
     onSubmit: async (values) => {
       if (values.file) {
@@ -72,16 +73,18 @@ const UploadForm: React.FC<UploadFormProps> = ({
       : null;
 
     if (file && !["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
-      setFileError("Only image files (jpg, png, gif) are allowed.");
+      setFileError("Μόνο εικονες (jpg, png, gif) μπορείτε να ανεβάσετε.");
       formik.setFieldValue("file", null);
       setUploadedFile(null);
       setPreviewImage(null);
+      setFileName(null); // Clear the file name
       return;
     }
 
     setFileError(null);
     formik.setFieldValue("file", file);
     setUploadedFile(file);
+    setFileName(file ? file.name : null); // Set the file name
 
     if (file) {
       const reader = new FileReader();
@@ -139,7 +142,7 @@ const UploadForm: React.FC<UploadFormProps> = ({
           htmlFor="file"
           className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none py-2 px-4 text-center"
         >
-          Διάλεξε Αρχείο
+          Διαλέξτε φωτογραφία
         </label>
         <input
           id="file"
@@ -149,6 +152,12 @@ const UploadForm: React.FC<UploadFormProps> = ({
           className="hidden"
         />
       </div>
+
+      {fileName && (
+        <div className="text-center text-sm text-gray-600 mt-2 break-words">
+          {fileName}
+        </div>
+      )}
 
       {fileError && <div className="text-red-600">{fileError}</div>}
       {formik.errors.file && (
@@ -165,39 +174,41 @@ const UploadForm: React.FC<UploadFormProps> = ({
         </div>
       )}
 
-      <button
-        type="submit"
-        className="px-4 py-2 bg-blue-500 text-white rounded-lg relative"
-        disabled={uploading || !!fileError}
-      >
-        {uploading ? (
-          <span className="flex items-center">
-            <svg
-              className="animate-spin h-5 w-5 mr-3"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 4.708c1.865 2.114 3 4.896 3 7.938h4c0-6.627-5.373-12-12-12v4c3.042 0 5.824 1.135 7.938 3l-2.647 3z"
-              ></path>
-            </svg>
-            Η φωτογραφία ανεβαίνει...
-          </span>
-        ) : (
-          "Ανεβάστε Φωτογραφία"
-        )}
-      </button>
+      <div className="grid grid-cols-1 gap-4">
+        <button
+          type="submit"
+          className="w-full h-12 bg-blue-500 text-white rounded-lg relative"
+          disabled={uploading || !!fileError}
+        >
+          {uploading ? (
+            <span className="flex items-center justify-center">
+              <svg
+                className="animate-spin h-5 w-5 mr-3"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 4.708c1.865 2.114 3 4.896 3 7.938h4c0-6.627-5.373-12-12-12v4c3.042 0 5.824 1.135 7.938 3l-2.647 3z"
+                ></path>
+              </svg>
+              Η φωτογραφία ανεβαίνει...
+            </span>
+          ) : (
+            "Ανεβάσε Φωτογραφία"
+          )}
+        </button>
+      </div>
 
       {uploadProgress > 0 && (
         <span className="flex items-center gap-x-3 whitespace-nowrap">
